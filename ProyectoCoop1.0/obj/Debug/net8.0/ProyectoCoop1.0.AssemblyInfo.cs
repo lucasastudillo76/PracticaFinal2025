2b48a26966dda7a87d40e4be7c7587e15ea93639
Lucas Astudillo <lucasastudillo76@gmail.com>
2025-09-04 20:34:16 -0300

    Correcion boton Cancelar, agregue tareas de cada servicio

--- ProyectoCoop1.0/obj/Debug/net8.0/ProyectoCoop1.0.AssemblyInfo.cs
+++ ProyectoCoop1.0/obj/Debug/net8.0/ProyectoCoop1.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoCoop1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be6fc2fab4baee6cb477b1c235dbe01738f4752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4217e30ab661a84d99d70a76e261ce82af74e67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoCoop1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoCoop1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
